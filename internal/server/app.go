fd1b275f166585fe945f2ffb237f81e593522cc3
Sven Strittmatter <ich@weltraumschaf.de>
2021-12-21 21:12:02 +0100

    Use a router
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/server/app.go
+++ internal/server/app.go
@@ -1,8 +1,14 @@
 package server
 
 import (
-"fmt"
-"github.com/urfave/cli/v2"
+	"encoding/json"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"log"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
 )
 
 func Create() *cli.App {
@@ -19,8 +25,72 @@ func Create() *cli.App {
 	}
 }
 
+type Article struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
+var Articles []Article
+
 func Execute(c *cli.Context) error {
-	fmt.Println("Hello, world!")
+	Articles = []Article{
+		Article{Id: 1, Title: "Hello", Description: "Article Description", Content: "Article Content"},
+		Article{Id: 2, Title: "Hello 2", Description: "Article Description", Content: "Article Content"},
+	}
+
+	handleRequests()
 	return nil
 }
 
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	for _, article := range Articles {
+		if article.Id == id {
+			json.NewEncoder(w).Encode(article)
+			break
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
