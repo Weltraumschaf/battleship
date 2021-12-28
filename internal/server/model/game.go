2373cc45822952b7dc7b3467894704fa21f41e69
Sven Strittmatter <ich@weltraumschaf.de>
2021-12-28 20:53:11 +0100

    Implement stringer for all models
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/server/model/game.go
+++ internal/server/model/game.go
@@ -1,7 +1,14 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type Game struct {
 	Id uuid.UUID `json:"id"`
+}
+
+func (g Game) String() string {
+	return fmt.Sprintf("{ID:%s}", g.Id)
 }
--- internal/server/model/user.go
+++ internal/server/model/user.go
@@ -1,8 +1,15 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type User struct {
-	Id uuid.UUID `json:"id"`
-	Name string `json:"name"`
+	Id   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("{ID:%s, Name:%s}", u.Id, u.Name)
 }
