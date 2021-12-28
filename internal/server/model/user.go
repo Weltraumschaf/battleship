f463288754ee1d68733d6ee46f37d4fb08442531
Sven Strittmatter <ich@weltraumschaf.de>
2021-12-28 21:59:17 +0100

    Remove unnecessary id property from user
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/server/model/user.go
+++ internal/server/model/user.go
@@ -2,14 +2,12 @@ package model
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 )
 
 type User struct {
-	Id   uuid.UUID `json:"id"`
 	Name string    `json:"name"`
 }
 
 func (u User) String() string {
-	return fmt.Sprintf("{ID:%s, Name:%s}", u.Id, u.Name)
+	return fmt.Sprintf("{Name:%s}", u.Name)
 }
