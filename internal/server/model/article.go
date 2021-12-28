6239c2173d8debe1427d8b48d4be0f99c178891c
Sven Strittmatter <ich@weltraumschaf.de>
2021-12-28 20:46:42 +0100

    Iplement stringer for logging
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- internal/server/model/article.go
+++ internal/server/model/article.go
@@ -1,10 +1,19 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type Article struct {
 	Id          uuid.UUID `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Content     string    `json:"content"`
+}
+
+func (a Article) String() string {
+	return fmt.Sprintf(
+		"{Id:%s, Title:%s, Description:%s, Content:%s}",
+		a.Id, a.Title, a.Description, a.Content)
 }
